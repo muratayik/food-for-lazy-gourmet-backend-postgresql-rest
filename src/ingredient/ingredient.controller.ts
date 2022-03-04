6475bdcafb3bc8635b6f7c89ec7856f6b884e85b
Murat Ayik <muratayik@murat.ayik>
2022-03-04 17:23:39 +0300

    Connect ingredients and meal

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,40 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
-import { CreateIngredientDto } from './dto/create-ingredient.dto';
-import { UpdateIngredientDto } from './dto/update-ingredient.dto';
-import { Ingredient } from './ingredient.entity';
-import { IngredientService } from './ingredient.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('ingredient')
-export class IngredientController {
-  constructor(private ingredientService: IngredientService) {}
-
-  @Get()
-  getIngredients(): Promise<Ingredient[]> {
-    return this.ingredientService.getIngredients();
-  }
-
-  @Get('/:id')
-  getIngredient(id: string): Promise<Ingredient> {
-    return this.ingredientService.getIngredient(id);
-  }
-
-  @Patch()
-  updateIngredient(
-    @Body() updateIngredientDto: UpdateIngredientDto,
-  ): Promise<Ingredient> {
-    return this.ingredientService.updateIngredient(updateIngredientDto);
-  }
-
-  @Delete('/:id')
-  deleteIngredient(@Param('id') id: string): Promise<void> {
-    return this.ingredientService.deleteIngredient(id);
-  }
-}
+export class IngredientController {}
